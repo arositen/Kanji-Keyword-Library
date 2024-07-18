86eefcca0e87faf799c427db30eb92da5a58f795
Jesse Guerrero <jesseguerrero.atx@gmail.com>
2024-07-18 15:32:23 -0600

    added option to load port from environment variable

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 
+const PORT = process.env.PORT ?? 3002;
+
 const app = express();
 
-app.get('/api/hi', (req, res) => res.send("hello there"));
+app.get('/api/hi', (req, res) => res.send("hello there Jesse"));
+app.get('/api/bye', (req, res) => res.send("Bye for now Jesse"));
 
-app.listen(3002, () => console.log("Express server has started and is listening on port 3002"));
+app.listen(PORT, () => console.log("Express server has started and is listening on port 3002"));
