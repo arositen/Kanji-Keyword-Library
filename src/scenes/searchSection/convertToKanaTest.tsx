169b5e353605acfac8767091a71fc0238a53436c
Jesse Guerrero <jesseguerrero.atx@gmail.com>
2024-08-12 16:48:42 -0600

    current progress of parsing romaji into kana

--- src/scenes/searchSection/convertToKanaTest.tsx
+++ src/scenes/searchSection/convertToKanaTest.tsx
@@ -5,8 +5,139 @@ const convertToKanaTest = () => {
     const [currentText, setCurrentText] = useState("");
     const [convertedText, setConvertedText] = useState("");
 
+    const parseIntoKana = (romaji: string): string => {
+
+        if (romaji === '') {
+            return '';
+        } else {
+            let characterArray = romaji.split('')
+            let parsedString = ''
+            let index = 0;
+            for (let i = 0; i < characterArray.length; i++) {
+
+                if (characterArray[i] in letterToKana) {
+                    parsedString = parsedString + letterToKana[characterArray[i]];
+                }
+            }
+            return parsedString
+        }
+
+    }
+
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setCurrentText(e.target.value);
+        setConvertedText(parseIntoKana(e.target.value));
+    }
+
+    const letterToKana = {
+        'a': 'あ',
+        'i': 'い',
+        'u': 'う',
+        'e': 'え',
+        'o': 'お',
+        'ka': 'か',
+        'ki': 'き',
+        'ku': 'く',
+        'ke': 'け',
+        'ko': 'こ',
+        'sa': '',
+        'shi': '',
+        'su': '',
+        'se': '',
+        'so': '',
+        'ta': '',
+        'chi': '',
+        'tsu': '',
+        'te': '',
+        'to': '',
+        'na': '',
+        'ni': '',
+        'nu': '',
+        'ne': '',
+        'no': '',
+        'ha': '',
+        'hi': '',
+        'fu': '',
+        'he': '',
+        'ho': '',
+        'ma': '',
+        'mi': '',
+        'mu': '',
+        'me': '',
+        'mo': '',
+        'ya': '',
+        'yu': '',
+        'yo': '',
+        'ra': '',
+        'ri': '',
+        'ru': '',
+        're': '',
+        'ro': '',
+        'wa': '',
+        'wo': '',
+        'kya': '',
+        'kyu': '',
+        'kyo': '',
+        'sha': '',
+        'shu': '',
+        'sho': '',
+        'cha': '',
+        'chu': '',
+        'cho': '',
+        'nya': '',
+        'nyu': '',
+        'nyo': '',
+        'hya': '',
+        'hyu': '',
+        'hyo': '',
+        'mya': '',
+        'myu': '',
+        'myo': '',
+        'rya': '',
+        'ryu': '',
+        'ryo': '',
+        'ga': '',
+        'gi': '',
+        'gu': '',
+        'ge': '',
+        'go': '',
+        'za': '',
+        'ji': '',
+        'zu': '',
+        'ze': '',
+        'zo': '',
+        'da': '',
+        'dji': '',
+        'dzu': '',
+        'de': '',
+        'do': '',
+        'ba': '',
+        'bi': '',
+        'bu': '',
+        'be': '',
+        'bo': '',
+        'pa': '',
+        'pi': '',
+        'pu': '',
+        'pe': '',
+        'po': '',
+        'n': '',
+        'gya': '',
+        'gyu': '',
+        'gyo': '',
+        'ja': '',
+        'ju': '',
+        'jo': '',
+        'dja': '',
+        'dju': '',
+        'djo': '',
+        'bya': '',
+        'byu': '',
+        'byo': '',
+        'pya': '',
+        'pyu': '',
+        'pyo': '',
+
     }
 
     return (
@@ -20,7 +151,7 @@ const convertToKanaTest = () => {
             <div className='flex flex-col justify-center basis-1/2 m-2'>
                 <h1 className='flex justify-center text-lg w-full'>This is the result section:</h1>
                 <div className='flex justify-center'>
-                    <div className='px-1 border-2 border-black w-60 h-8'>{currentText}</div>
+                    <div className='px-1 border-2 border-black w-60 h-8'>{convertedText}</div>
                 </div>
             </div>
         </section>
