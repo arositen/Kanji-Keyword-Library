ef21c944bbedf65913d6c669ef6e14f55d869f1e
Jesse Guerrero <jesseguerrero.atx@gmail.com>
2024-09-16 14:50:37 -0600

    added preliminary context provider for theme

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import SearchSection from "@/scenes/searchSection";
 function App() {
 
   return (
-    <div className='flex flex-col justify-center max-w-[1200px] mx-auto relative bg-DarkMode-Gray-600'>
-      <KKDLoadingScreen />
-      <Navbar />
-      <SearchSection />
+    <div className='w-full h-full bg-[#212529]'>
+      <div className='flex flex-col justify-center max-w-[1200px] mx-auto relative bg-DarkMode-Gray-600'>
+        <KKDLoadingScreen />
+        <Navbar />
+        <SearchSection />
+      </div>
     </div>
 
   )
---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, ReactNode, useState } from "react";
+
+type ThemeContextProviderProps = {
+    children: ReactNode;
+}
+
+type ThemeContext = {
+    themeMode: string,
+    changeTheme: () => void,
+}
+
+const ThemeContext = createContext({} as ThemeContext);
+
+export function useTheme() {
+    return useContext(ThemeContext);
+}
+
+const ThemeContextProvider = ({ children }: ThemeContextProviderProps) => {
+
+    const [themeMode, setTheme] = useState('light');
+
+    const changeTheme = () => {
+        if (themeMode === "light") {
+            setTheme("dark")
+        } else if (themeMode === "dark") {
+            setTheme("light")
+        }
+    }
+
+    return (
+        <ThemeContext.Provider
+            value={{
+                themeMode,
+                changeTheme,
+            }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeContextProvider
